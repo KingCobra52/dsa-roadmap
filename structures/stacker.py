c5a4f5db9e0abd1703f2ab7157c08d80672514f9
Siddarth Thota <hellotheking52@gmail.com>
2026-08-16 11:48:58 -0400

    Remove venv from version control

--- structures/stacker.py
+++ structures/stacker.py
@@ -1,18 +1,24 @@
 class Stack:
-     def __init__(self):
-         self.items = []
+    def __init__(self):
+        self.items = []
 
-     def isEmpty(self):
-         return self.items == []
+    def isEmpty(self):
+        return self.items == []
 
-     def push(self, item):
-         self.items.append(item)
+    def push(self, item):
+        self.items.append(item)
 
-     def pop(self):
-         return self.items.pop()
+    def pop(self):
+        self.items.pop()
 
-     def peek(self):
-         return self.items[len(self.items)-1]
+    def peek(self):
+        return self.items[-1]
 
-     def size(self):
-         return len(self.items)
+    def size(self):
+        return len(self.items)
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
