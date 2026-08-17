84fefbf909659eceedcadfeeeb3bfb610198aa4e
Siddarth Thota <hellotheking52@gmail.com>
2026-08-17 16:08:16 -0400

    added some structures working on others

--- structures/uno.py
+++ structures/uno.py
@@ -3,10 +3,14 @@ class Node:
         self.data = data
         self.next = None
 
-
 class LinkedList:
     def __init__(self) -> None:
         self.head = None
+
+    def add(self, data):
+        temp = Node(data)
+        temp.next = self.head
+        self.head = temp
 
     def size(self):
         count = 0
@@ -16,3 +20,56 @@ class LinkedList:
             current = current.next
 
         return count
+
+    def search(self, item):
+        current = self.head
+        while current is not None:
+            if current.data == item:
+                return True
+            current = current.next
+        return False
+
+    def remove(self, item):
+        previous = None
+        current = self.head
+        while current is not None:
+            if current.data == item:
+                if previous is not None:
+                    previous.next = current.next
+                    return True
+                else:
+                    self.head = current.next
+                    return True
+            previous = current
+            current = current.next
+        return False
+
+    def append(self, item):
+        #add new node to the end of list
+        temp = Node(item)
+        if self.head is None:
+            self.head = temp
+        else:
+            current = self.head
+            while current.next is not None:
+                current = current.next
+            current.next = temp
+
+    def insert(self, pos, item):
+        node = Node(item)
+        previous = None
+        current = self.head
+        count = 0
+        while current is not None and count < pos:
+            pass
+
+def main():
+    mylist = LinkedList()
+    mylist.add(31)
+    mylist.add(77)
+    mylist.add(17)
+    print(mylist.head.data)
+    print(mylist.head.next.next.data)
+
+if __name__ == "__main__":
+    main()
